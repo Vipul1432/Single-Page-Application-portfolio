20a15e4f49a5f20279033eda85551bec057f4476
Mr.Vipul <vipulupadhyay563@gmail.com>
2023-03-18 10:14:22 +0530

    Add assets

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+
+var typed = new Typed(".soft-dev", {
+    strings: ['Software Developer', 'Web Developer'],
+    typeSpeed: 100,
+    backSpeed: 50,
+    loop: true
+})
+
+var typed = new Typed(".web-dev", {
+    strings: ['Software Developer', 'Web Developer'],
+    typeSpeed: 100,
+    backSpeed: 50,
+    loop: true
+})
